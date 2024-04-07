806bfc8793406579fded2a028ab530a37349f304
RodRibDev <rodrigo_r_ribeiro@estudante.sesisenai.org.br>
2024-04-07 19:45:30 -0300

    Exercício 02

--- app.js
+++ app.js
@@ -2,6 +2,18 @@ const express = require("express")
 
 const app = express();
 
+app.get("/", function(req, res){
+    res.send("APP da aula 09")
+})
+
+app.get("/sobre", function(req, res){
+    res.send("Este é meu APP da aula 09, criado nos exercícios da semanado Trello!")
+})
+
+app.get("/contato", function(req, res){
+    res.send("Entre em contato para maiores informações")
+})
+
 app.listen(3000, function(){
     console.log("Minha APP está no ar!")
 });
